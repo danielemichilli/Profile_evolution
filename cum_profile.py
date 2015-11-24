81917d12734e63a89a32e1da7aac69d5ea8e718f
Daniele Michilli <michilli@drg21.control.lofar>
2015-11-24 23:55:17 +0000

    Limits added

--- cum_profile.py
+++ cum_profile.py
@@ -25,7 +25,7 @@ product_folder  = home_folder + '/Products'
 plot_folder     = home_folder + '/Plots'
 profile_template = home_folder + '/ephemeris/151109_profile_template.std'
 
-def create_profile(args,verbose,overwrite,loud,parallel):
+def create_profile(args,verbose,overwrite,loud,parallel,exclude=False):
   """
   Process a list of fits file to produce cleaned archives.
   Plot the profiles of the archives both singularly and together.
@@ -43,7 +43,9 @@ def create_profile(args,verbose,overwrite,loud,parallel):
     obs_list = [file for file in obs_list for obs in args if obs in file]
     if loud: 
       if len(obs_list) != len(args): print "WARNING: Number of submitted and found observations don't match. Pheraps some observation is missing\n"
-      
+  if exclude:
+    obs_list = [x for x in obs_list if not x in exclude]
+
   print "The following observations will be processed:\n{}\n".format(obs_list)
   
   #Set the verbosity level
@@ -281,10 +283,13 @@ def zap_channels(file):
   return zap_channels#, obs_date
   
 
-def plot_lists():
+def plot_lists(exclude=False,date_lim=False):
   date_list = []
   obs_list = []
   for obs in os.listdir(product_folder):
+    if exclude:
+      if obs in exclude:
+        continue
     if os.path.isdir(os.path.join(product_folder,obs)):
       archive = '{}/{}/{}_correctDM.clean.TF.ar'.format(product_folder,obs,obs)
 
@@ -296,12 +301,14 @@ def plot_lists():
         prof = np.roll(prof,(len(prof)-np.argmax(prof))+len(prof)/2)
         epoch = load_archive.get_Integration(0).get_epoch()
         date = datetime.date(int(epoch.datestr('%Y')), int(epoch.datestr('%m')), int(epoch.datestr('%d')))
+        if date_lim:
+          if not date_lim[0] <= date <= date_lim[1]:
+            continue
         if date in date_list:
           obs_list[date_list.index(date)] += prof
         else:
           date_list.append(date)
           obs_list.append(prof)
-        #print obs[1:],date
   for obs in obs_list:
     obs -= np.median(obs)
     obs /= np.max(obs)
@@ -309,54 +316,81 @@ def plot_lists():
   return date_list,obs_list
 
 
-def cum_plot(zoom=False):
-  date_list,obs_list = plot_lists()
+def cum_plot(phase_lim=False,date_lim=False,flux_lim=False,exclude=False):
+  date_list,obs_list = plot_lists(exclude,date_lim)
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+
+  base_y = np.min([date.toordinal() for date in date_list])
+  scale_y = 0.2 / np.min(np.diff(sorted(date_list))).days
+  obs_max = 0
+  obs_min = 1
   for idx,obs in enumerate(obs_list):
-    plt.plot(np.arange(0,1,1./len(obs)),obs,label=int(date_list[idx]))
+    x = np.arange(0,1,1./len(obs))
+    if phase_lim:
+      zoom_idx = np.where((x>=min(phase_lim[0],phase_lim[1]))&(x<=max(phase_lim[0],phase_lim[1])))
+      x = x[zoom_idx]
+      obs = obs[zoom_idx]
+    if flux_lim: obs = np.clip(obs,flux_lim[0],flux_lim[1])
+    obs += scale_y * (float(date_list[idx].toordinal()) - base_y)
+    obs_max = max(obs_max, max(obs))
+    obs_min = min(obs_min, min(obs))
+    ax.plot(x, obs, color='k')
 
-  if zoom:
-    plt.xlim([0.51,0.55])
-    plt.ylim([-0.01,0.20])
-  plt.legend(sorted(date_list))
-  plt.xlabel('Phase')
-  plt.ylabel('Rel. amplitude')
+  if phase_lim: ax.set_xlim([phase_lim[0],phase_lim[1]])
+  else: ax.set_xlim([0,1])
+  if flux_lim: ax.set_ylim([flux_lim[0],flux_lim[1]])
+  else: ax.set_ylim([0,1])
+
+  ax.set_xlabel('Phase')
+  ax.set_ylabel('Date')
+  glitch_epoch = datetime.date(2011, 10, 25).toordinal()
+  if phase_lim: ax.scatter(phase_lim[1],scale_y * (glitch_epoch - base_y),c='r',marker='o',linewidth=0,s=12)
+  else: ax.scatter(1,scale_y * (glitch_epoch - base_y),c='r',marker='o',linewidth=0,s=12)
+
+  #Convert dates from ordinals
+  fig.canvas.draw()
+  #ax.set_yticklabels([datetime.date.fromordinal(int((day+base_y)/scale_y)).strftime('%d-%m-%Y') for day in ax.get_yticks()])
+  ax.tick_params(axis='y', labelsize=6)
   #Save the plot
-  if zoom: plot_name = 'cum_profile_zoom'
-  else: plot_name = 'cum_profile'
-  plt.savefig('{}/{}_{}.png'.format(plot_folder,time.strftime("%Y%m%d"),plot_name),format='png',dpi=200)
+  plot_name = 'profiles'
+  plt.savefig('{}/{}_{}.png'.format(plot_folder,time.strftime("%Y%m%d-%H%M%S"),plot_name),format='png',dpi=200)
 
 
-def plot3D(zoom=False):
-  date_list,obs_list = plot_lists()
+
+def plot3D(phase_lim=False,date_lim=False,flux_lim=False,exclude=False):
+  date_list,obs_list = plot_lists(exclude,date_lim)
   fig = plt.figure()
   ax = fig.add_subplot(111, projection='3d')
 
   for idx,obs in enumerate(obs_list):
     x = np.arange(0,1,1./len(obs))
-    if zoom:
-      zoom_idx = np.where((x>=0.51)&(x<=0.6))
+    if phase_lim:
+      zoom_idx = np.where((x>=min(phase_lim[0],phase_lim[1]))&(x<=max(phase_lim[0],phase_lim[1])))
       x = x[zoom_idx]
       obs = obs[zoom_idx]
-      obs = np.clip(obs,-0.1,0.05)
+    if flux_lim: obs = np.clip(obs,flux_lim[0],flux_lim[1])
     ax.plot(x, [float(date_list[idx].toordinal()),]*x.size,obs,color='k')
   
-  if zoom:
-    ax.set_xlim([0.51,0.6])
-    ax.set_ylim([min(date_list),max(date_list)])
-    ax.set_zlim([-0.01,0.05])
+  if phase_lim: ax.set_xlim3d([phase_lim[0],phase_lim[1]])
+  else: ax.set_xlim3d([0,1])
+  if flux_lim: ax.set_zlim3d([flux_lim[0],flux_lim[1]])
+  else: ax.set_zlim3d([0,1])
+  ax.set_ylim3d([min(date_list).toordinal(),max(date_list).toordinal()])
+
   ax.set_xlabel('Phase')
-  ax.set_ylabel('Date (MJD)')
+  ax.set_ylabel('Date')
   ax.set_zlabel('Rel. amplitude')
   glitch_epoch = datetime.date(2011, 10, 25).toordinal()
-  ax.plot((x.max(),x.max()),(glitch_epoch,glitch_epoch),(0,0),'ro',linewidth=0,markersize=12)
+  if phase_lim: ax.scatter(phase_lim[1],glitch_epoch,0,c='r',marker='o',linewidth=0,s=12)
+  else: ax.scatter(1,glitch_epoch,0,c='r',marker='o',linewidth=0,s=12)
   #Convert dates from ordinals
   fig.canvas.draw()
   ax.set_yticklabels([datetime.date.fromordinal(int(day+1)).strftime('%d-%m-%Y') for day in ax.get_yticks()])
   ax.tick_params(axis='y', labelsize=6)
   #Save the plot
-  if zoom: plot_name = '3D_profiles_zoom'
-  else: plot_name = '3D_profiles'
-  plt.savefig('{}/{}_{}.png'.format(plot_folder,time.strftime("%Y%m%d"),plot_name),format='png',dpi=200)
+  plot_name = '3D_profiles'
+  plt.savefig('{}/{}_{}.png'.format(plot_folder,time.strftime("%Y%m%d-%H%M%S"),plot_name),format='png',dpi=200)
 
 
 
@@ -369,23 +403,29 @@ if __name__ == '__main__':
     Plot the profiles of the archives both singularly and together.
     Process all the observations unless specific names are given as args.
     """)
-  parser.add_argument('-f','-folder', nargs='+', help='List of specific observations to process. Format: LXXXXX. All the observations will be processed if empty')
+  parser.add_argument('-f','-folder', nargs='+', help='List of specific observations to process. Format: LXXXXX. All the observations will be processed by default if empty')
+  parser.add_argument('-e','-exclude', nargs='+', help='List of specific observations to exclude from the process. Format: LXXXXX')
   parser.add_argument('-p','-plot', action='store_true', help='Produce a cumulative plot of all the observations without process them')
   parser.add_argument('-p3d','-plot3d', action='store_true', help='Produce a tridimensional plot of all the observations without process them')
-  parser.add_argument('--zoom', nargs='+', help='Zoom on pulse parts. The first to values must be the pulse phase limits, the third (optional) is the maximum flux value')
+  parser.add_argument('--phase_lim', nargs=2, type=float, help='Limits on the pulse phase')
+  parser.add_argument('--date_lim', nargs=2, help='Limits on the date (DD-MM-YYYY)')
+  parser.add_argument('--flux_lim', nargs=2, type=float, help='Limits on the flux (rel. units)')
   parser.add_argument('-ow','-overwrite', action='store_true', help='Reprocess all the observations present. ATTENTION: the content will be overwritten!')  
   parser.add_argument('--parallel', action='store_true', help='Process the observations on multiple CPUs')
   parser.add_argument('-v','-verbose', action='store_true', help='Verbose output')
   parser.add_argument('-q','-quiet', action='store_false', help='Quiet output')
   args = parser.parse_args()
-  
+ 
+  if args.date_lim: date_lim = [datetime.datetime.strptime(x,'%d-%m-%Y').date() for x in args.date_lim]
+  else: date_lim = False
+
   #Produce the cumulative plot and exit if the plot argument is given
   if args.p | args.p3d:
-    if args.p: cum_plot(zoom = args.zoom)
-    if args.p3d: plot3D(zoom = args.zoom)
+    if args.p: cum_plot(exclude=args.e,phase_lim=args.phase_lim,date_lim=date_lim,flux_lim=args.flux_lim)
+    if args.p3d: plot3D(exclude=args.e,phase_lim=args.phase_lim,date_lim=date_lim,flux_lim=args.flux_lim)
     exit()
     
-  create_profile(args.f,args.v,args.ow,args.q,args.parallel)
+  create_profile(args.f,args.v,args.ow,args.q,args.parallel,exclude=args.e)
   
   if args.q: print """
   
